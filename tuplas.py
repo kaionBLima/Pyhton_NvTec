507cb48a66ffcc64e9052acf8451ee97b0705012
kaionBLima <kaionbrandaolima@gmail.com>
2025-03-19 11:21:11 -0300

    Trab. com tuplas em python

---
+++ tuplas.py
@@ -0,0 +1,7 @@
+#tuplas são imuáveis, não tem como adcionar ou remover itens
+
+my_tuple = (1, 2, 3, 4, 5);
+print(my_tuple.index(1));
+print(my_tuple.count(1));
+
+#Tem as mesmas funcoes que as listas, mas não pode adicionar ou remover itens
