5e9c30ae6acb103812b32646eda45e5f8d6fec1b
kaionBLima <kaionbrandaolima@gmail.com>
2025-06-04 10:55:49 -0300

    feat(DjangoDB02):Update-project-DjangoDB02

--- DjangoDB02/todoApp/todo_list/views.py
+++ DjangoDB02/todoApp/todo_list/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import todo
+from .forms import TodoForm
 
 def home(request):
-    todos = todo.objects.all()
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        form = TodoForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            todos = todo.objects.all()
+            return render(request, 'home.html', {'todos': todos})
+        else:
+            todos = todo.objects.all()
+            return render(request, 'home.html', {'todos': todos})
 
 def about(request):
     context = {'name': 'John Dow', 'age': 30}
